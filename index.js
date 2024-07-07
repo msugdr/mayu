d8893c668f8f5fb383f50b6a208345c849cb13ec
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-07 17:50:49 +0900

    Update index.js wiki AKB48

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer(async (request, response) => {
     try {
-        const sz = await wiki();
+        const sz = await wiki("AKB48");
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.end("Hello World Mayu!!\n" + sz);
     } catch (error) {
@@ -13,9 +13,23 @@ const server = http.createServer(async (request, response) => {
 
 server.listen(process.env.PORT);
 
-async function wiki() {
+async function wiki(keyword) {
 //    const url = "http://ip.jsontest.com/";
-    const url = "http://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&rvprop=content&titles=AKB48";
+//    const url = "http://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&rvprop=content&titles=AKB48";
+    const urlBase="https://ja.wikipedia.org/w/api.php?origin=*";
+    var urlPara={
+      format:"json",
+      action:"query",
+      prop:"extracts",
+      explaintext:"",
+      exintro:"",
+      titles:"AKB48"
+    };
+    urlPara["titles"]=keyword;
+    var url=urlBase;
+    Object.keys(urlPara).forEach(function(key){
+      url+= "&" + key + "=" + urlPara[key];
+    });
     const res = await fetch(url);
     const dat = await res.text();
     console.log(dat);
