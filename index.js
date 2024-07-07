22559fa8de16c5f34f9bfbc14a8ecdb0b480bea5
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-07 11:35:26 +0900

    Update index.js wiki

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
-  response.end("Hello World\n");
+  response.end("Hello World\n" + wiki());
 });
 server.listen(process.env.PORT);
+
+function wiki() {
+  const url = "http://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&rvprop=content&titles=AKB48";
+   fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        const sz=data;
+           }); 
+  return sz;
+}
