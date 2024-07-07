b03079857acefb73ee0bbe3d6d6ebbd4d413bce0
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-07 17:39:31 +0900

    Update index.js wiki

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const http = require('http');
-const server = http.createServer((request, response) => {
-  response.end("Hello World\n" + wiki());
+
+const server = http.createServer(async (request, response) => {
+    try {
+        const sz = await wiki();
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end("Hello World Mayu!!\n" + sz);
+    } catch (error) {
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end("Error: " + error.message);
+    }
 });
+
 server.listen(process.env.PORT);
 
-function wiki() {
-  const url = "http://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&rvprop=content&titles=AKB48";
-   fetch(url)
-    .then(response => response.text())
-    .then(data => {
-        const sz=data;
-           }); 
-  return sz;
+async function wiki() {
+    const url = "http://ip.jsontest.com/";
+    const res = await fetch(url);
+    const dat = await res.text();
+    console.log(dat);
+    return dat;
 }
