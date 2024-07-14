308db6fcb1336211685fd53d966d3c34c088e29b
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-14 21:13:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const server = http.createServer((request, response) => {
-  response.end("Hello World\n");
+const json = { name: "Youna", age: 18, sex: "female" };
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(json));
 });
 server.listen(process.env.PORT);
