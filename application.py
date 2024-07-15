a77f0280caa8daafc04c8110e03df79ec0d65fb2
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-15 10:40:29 +0900

    Update and rename main.py to application.py

--- application.py
+++ application.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def get_json():
+def main_page():
     return "Hello Youna!!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
