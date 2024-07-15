e8deba49a84abecc00871bc5ba2c39cbfac26d12
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-15 11:04:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
+    json_data = {"name": "Youna", "age": 18, "sex": "female"}
+    return jsonify(json_data)
     return "Hello Youna!!"
