57e81041a4014026c5f651a636e1274b74cc5e0a
msugdr <99190980+msugdr@users.noreply.github.com>
2024-07-15 09:16:33 +0900

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const json = { name: "Youna", age: 18, sex: "female" };
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(json));
-});
-server.listen(process.env.PORT);
